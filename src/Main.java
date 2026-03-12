adc77d027b3758df97e05199c2c7d88f8b8aaa7e
Filip Wisniowski <filipwisniowski@filips-air.wireless.hhs.nl>
2026-03-12 10:41:19 +0100

    LayoutLoader en HotelEvents jar toegevoegd

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import model.Hotel;
+import model.LayoutLoader;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            Hotel hotel = LayoutLoader.laadLayout("layouts/hotel1.layout");
+            System.out.println("Hotel geladen!");
+            System.out.println("Breedte: " + hotel.getBreedte());
+            System.out.println("Hoogte: " + hotel.getHoogte());
 
+            // Print het grid
+            for (String[] rij : hotel.getGrid()) {
+                for (String vakje : rij) {
+                    System.out.print(vakje + " ");
+                }
+                System.out.println();
+            }
+        } catch (Exception e) {
+            System.out.println("Fout: " + e.getMessage());
+        }
     }
 }
---
+++ src/model/LayoutLoader.java
@@ -0,0 +1,49 @@
+package model;
+
+import java.io.*;
+import java.util.*;
+
+public class LayoutLoader {
+
+    public static Hotel laadLayout(String bestandspad) throws Exception {
+        // Controleer of het bestand .layout extensie heeft
+        if (!bestandspad.endsWith(".layout")) {
+            throw new Exception("Ongeldig bestandstype. Alleen .layout bestanden zijn toegestaan.");
+        }
+
+        List<String[]> rijen = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(bestandspad));
+        String lijn;
+
+        while ((lijn = reader.readLine()) != null) {
+            rijen.add(lijn.split(","));
+        }
+        reader.close();
+
+        int hoogte = rijen.size();
+        int breedte = rijen.get(0).length;
+        String[][] grid = rijen.toArray(new String[0][]);
+        valideerLayout(grid);
+
+        return new Hotel(grid, breedte, hoogte);
+
+
+    }
+    private static void valideerLayout(String[][] grid) throws Exception {
+        boolean heeftLobby = false;
+        boolean heeftLift = false;
+        boolean heeftTrap = false;
+
+        for (String[] rij : grid) {
+            for (String vakje : rij) {
+                if (vakje.equals("L")) heeftLobby = true;
+                if (vakje.equals("F")) heeftLift = true;
+                if (vakje.equals("T")) heeftTrap = true;
+            }
+        }
+
+        if (!heeftLobby) throw new Exception("Layout mist een lobby (L)!");
+        if (!heeftLift) throw new Exception("Layout mist een lift (F)!");
+        if (!heeftTrap) throw new Exception("Layout mist een trap (T)!");
+    }
+}
